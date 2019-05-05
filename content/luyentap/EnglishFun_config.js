38c7bf4d74cd089a84d9afe45bd4c53b31559f38
meomen <vuducminhkyle@gmail.com>
2019-05-05 17:38:25 +0700

    update

---
+++ content/luyentap/EnglishFun_config.js
@@ -0,0 +1,54 @@
+var typeOfQuestions = new Array();
+var questions = new Array();
+var choices = new Array();
+var answers = new Array();
+var response = new Array();
+
+questions[0] = "The two parties agreed to _______ the judge's decision";
+typeOfQuestions[0] = "Chọn từ thích hợp điền vào chỗ trống";
+choices[0] = new Array();
+choices[0][0] = "abide by";
+choices[0][1] = "agreement";
+choices[0][2] = "assurance";
+choices[0][3] = "cancellation";
+answers[0] = choices[0][0];
+
+questions[1] = "determine "
+typeOfQuestions[1] = "Chọn nghĩa của từ";
+choices[1] = new Array();
+choices[1][0] = "xác định";
+choices[1][1] = "tuân theo";
+choices[1][2] = "chứng minh";
+choices[1][3] = "từ bỏ";
+answers[1] = choices[1][0];
+
+questions[2] = "https://600tuvungtoeic.com/template/english/images/study/engage.jpg";
+typeOfQuestions[2] = "Chọn từ miêu tả hình ảnh";
+choices[2] = new Array();
+choices[2][0] = "establish";
+choices[2][1] = "obligate";
+choices[2][2] = "party";
+choices[2][3] = "engage";
+answers[2] = choices[2][3];
+
+questions[3] = "Giải quyết";
+typeOfQuestions[3] = "Điền từ tiếng anh phù hợp với nghĩa cho trước";
+answers[3] = "resolve";
+
+questions[4] = "https://600tuvungtoeic.com/template/english/images/study/specific.jpg";
+typeOfQuestions[4] = "Chọn từ miêu tả hình ảnh";
+choices[4] = new Array();
+choices[4][0] = "specific";
+choices[4][1] = "abide";
+choices[4][2] = "ago";
+choices[4][3] = "bother";
+answers[4] = choices[4][0];
+
+questions[5] = "The father made _____ for his children through his will.";
+typeOfQuestions[5] = "Chọn từ thích hợp điền vào chỗ trống";
+choices[5] = new Array();
+choices[5][0] = "provision";
+choices[5][1] = "abide by";
+choices[5][2] = "specific";
+choices[5][3] = "resolve";
+answers[5] = choices[5][0];
---
+++ content/luyentap/EnglishFun_functions.js
@@ -0,0 +1,144 @@
+var count = -1;
+var diem = 0;
+function render(){
+	if(count == (questions.length -1) ){
+		document.getElementById("end").innerHTML = "Bạn đã hoàn thành bài luyện tập";
+		return true;
+	}
+
+	count++;
+	document.getElementById("demo").innerHTML = " ";
+	document.getElementById("a").checked = false;
+	document.getElementById("b").checked = false;
+	document.getElementById("c").checked = false;
+	document.getElementById("d").checked = false;
+
+	document.getElementById("h-typeOfQuestions").innerHTML = typeOfQuestions[count];
+	if(typeOfQuestions[count] == "Chọn từ miêu tả hình ảnh"){
+		document.getElementById("h-questions").innerHTML = " ";
+        document.getElementById("anh").style.display = "inline-block";
+		document.getElementById("anh").src = questions[count];
+		document.getElementById("anh").width = "200";
+		document.getElementById("anh").height = "200";
+
+    } else if(typeOfQuestions[count] == "Điền từ tiếng anh phù hợp với nghĩa cho trước"){
+        document.getElementById("h-questions").innerHTML = questions[count];
+    	document.getElementById("blank").style.display = "inline-block";
+        document.getElementById("submit").style.display = "inline-block";
+        document.getElementById("a").style.display = "none";
+    	document.getElementById("b").style.display = "none";
+    	document.getElementById("c").style.display = "none";
+    	document.getElementById("d").style.display = "none";
+        document.getElementById("0").innerHTML = " ";
+        document.getElementById("1").innerHTML = " ";
+        document.getElementById("2").innerHTML = " ";
+        document.getElementById("3").innerHTML = " ";
+        
+        document.getElementById("anh").style.display = "none";
+
+
+        document.getElementById("cauthu").innerHTML = count + 1 + "/" + questions.length;
+
+        return true;
+
+        
+    }else {
+		document.getElementById("h-questions").innerHTML = questions[count];
+        document.getElementById("anh").style.display = "none";
+
+	} 
+    document.getElementById("blank").style.display = "none";
+    document.getElementById("submit").style.display = "none";
+    
+    document.getElementById("a").style.display = "inline-block";
+    document.getElementById("b").style.display = "inline-block";
+	document.getElementById("c").style.display = "inline-block";
+    document.getElementById("d").style.display = "inline-block";
+    
+	document.getElementById("a").disabled = false;
+	document.getElementById("b").disabled = false;
+	document.getElementById("c").disabled = false;
+	document.getElementById("d").disabled = false;
+
+	document.getElementById("0").innerHTML = choices[count][0];
+	document.getElementById("1").innerHTML = choices[count][1];
+	document.getElementById("2").innerHTML = choices[count][2];
+	document.getElementById("3").innerHTML = choices[count][3];
+
+	document.getElementById("a").value = choices[count][0];
+	document.getElementById("b").value = choices[count][1];
+	document.getElementById("c").value = choices[count][2];
+	document.getElementById("d").value = choices[count][3];
+    
+console.log("fb");
+	document.getElementById("cauthu").innerHTML = count + 1 + "/" + questions.length;
+}
+
+function submitAnswer(){
+	var c;
+	useranswers = document.getElementsByName("gender");
+	//
+	for(var i =0;i< useranswers.length; i++){
+		if(useranswers[i].checked){
+			c = useranswers[i].value;
+		} else {
+			useranswers[i].disabled = true;
+		}
+	}
+		//kiem tra ket qua cua cau tr loi
+		if(c == answers[count]){
+			console.log("helpp");
+			document.getElementById("demo").innerHTML = "Đúng rồi bạn. Giỏi quá!!";
+			document.getElementById("demo").style.color = "green";
+			document.getElementById("demo").style.fontSize= "xx-large";
+
+			console.log(diem);
+			diem = diem + 1;
+			document.getElementById("diem").innerHTML = diem;
+		} 
+		else{
+			document.getElementById("demo").innerHTML = "Ồ..sai rồi kiểm tra lại nhé! <br> Đáp án đúng là: " + answers[count];
+			document.getElementById("demo").style.color = "red";
+			document.getElementById("demo").style.fontSize = 'xx-large';
+		}
+}
+
+// function render2(){
+// 	if(count == (questions.length -1) ){
+// 		document.getElementById("end").innerHTML = "Bạn đã hoàn thành khóa học";
+// 		return true;
+// 	}
+
+// 	count++;
+// 	document.getElementById("demo").innerHTML = " ";
+
+// 	document.getElementById("h-typeOfQuestions").innerHTML = typeOfQuestions[count];
+// 	document.getElementById("h-questions").innerHTML = questions[count];
+// 	document.getElementById("cauthu").innerHTML = count + 1 + "/" + questions.length;
+	
+// }
+
+function submitAnswer2(){
+	var c;
+	useranswers = document.getElementById("blank");
+	
+		//kiem tra ket qua cua cau tr loi
+		c = useranswers.value;
+	
+		if(c == answers[count]){
+			document.getElementById("demo").innerHTML = "Bạn trả lời đúng rồi!";
+			document.getElementById("demo").style.color = "green";
+			document.getElementById("demo").style.fontSize= "xx-large";
+
+			diem = diem + 1;
+			document.getElementById("diem").innerHTML = diem;
+		} 
+		else{
+			document.getElementById("demo").innerHTML = "Bạn trả lời sai rồi! <br> Đáp án đúng là: " + answers[count];
+			document.getElementById("demo").style.color = "red";
+			document.getElementById("demo").style.fontSize = 'xx-large';
+		}
+}
+
+
+
